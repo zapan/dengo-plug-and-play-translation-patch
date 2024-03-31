e7ba42ffce1735386c63116e27484d6ceb1fa2b8
Zapan <mantillos-estelar0n@icloud.com>
2024-03-31 12:17:34 +0200

    Densha de Go Plug & play translation

--- hook_magic_36558b710a.sh
+++ hook_magic_36558b710a.sh
@@ -35,6 +35,8 @@ if [ -f "${USB_ROOT}/backup_required" ]; then
     # Generate MD5 sum of the tarball
     cd "${USB_ROOT}"
     md5sum dgtyp3zzzz.tar.gz > dgtyp3zzzz.tar.gz.md5
+
+
     # Generate MD5 sum of the installed files (per factory install script)
     echo `(find /root/Data/ /root/dgf -type f -exec md5sum {} \;) | awk '{print $1}' | env LC_ALL=C sort | md5sum` > installed.md5
     # Remove backup flag if successful
@@ -44,79 +46,118 @@ fi
 # Remount with write access
 mount -o remount,rw /
 
-if [ -f "${USB_ROOT}/revert" ]; then
-    # Revert changes flag detected, restore original file and delete chimes
-    cd /root
-    if [ ! -f dgf.orig ]; then
-        echo "Original dgf executable not found, cannot revert."
-        error_exit
-    fi
-    mv dgf.orig dgf
-    rm -rf Data/cddata/dengo
-    rm "${USB_ROOT}/revert"
+if [ -f "${USB_ROOT}/backup_translation" ]; then
+
+    echo "Translation files backup..."
+
+    cd /root/Data/cddata
+
+    mkdir -p "${USB_ROOT}/backup/cddata/2d"
+    mkdir -p "${USB_ROOT}/backup/cddata/common"
+    mkdir -p "${USB_ROOT}/backup/cddata/menu"
+
+    cp 2d/end_mem.dat       "${USB_ROOT}/backup/cddata/2d/"
+    cp 2d/game2d_mem.dat    "${USB_ROOT}/backup/cddata/2d/"
+    cp 2d/game2d_vram.dat   "${USB_ROOT}/backup/cddata/2d/"
+    cp common/com_mem.dat   "${USB_ROOT}/backup/cddata/common/"
+    cp menu/menu_mem_fj.dat "${USB_ROOT}/backup/cddata/menu/"
+    cp menu/menu_mem_us.dat "${USB_ROOT}/backup/cddata/menu/"
+
+    cp 2d/end_mem.dat       2d/end_mem.orig
+    cp 2d/game2d_mem.dat    2d/game2d_mem.orig
+    cp 2d/game2d_vram.dat   2d/game2d_vram.orig
+    cp common/com_mem.dat   common/com_mem.orig
+    cp menu/menu_mem_fj.dat menu/menu_mem_fj.orig
+    cp menu/menu_mem_us.dat menu/menu_mem_us.orig
+
+    chmod 664 2d/end_mem.orig
+    chmod 664 2d/game2d_mem.orig
+    chmod 664 2d/game2d_vram.orig
+    chmod 664 common/com_mem.orig
+    chmod 664 menu/menu_mem_fj.orig
+    chmod 664 menu/menu_mem_us.orig
+
+    chown 1000:1000 2d/end_mem.orig
+    chown 1000:1000 2d/game2d_mem.orig
+    chown 1000:1000 2d/game2d_vram.orig
+    chown 1000:1000 common/com_mem.orig
+    chown 1000:1000 menu/menu_mem_fj.orig
+    chown 1000:1000 menu/menu_mem_us.orig
+
+    # Remove backup flag if successful
+    rm "${USB_ROOT}/backup_translation"
+
+    echo "Translation files backup OK."
+fi
+
+
+if [ -f "${USB_ROOT}/revert_translation" ]; then
+
+    echo "Reverting translation files..."
+
+    cd /root/Data/cddata
+
+    mv 2d/end_mem.orig        2d/end_mem.dat
+    mv 2d/game2d_mem.orig     2d/game2d_mem.dat
+    mv 2d/game2d_vram.orig    2d/game2d_vram.dat
+    mv common/com_mem.orig    common/com_mem.dat
+    mv menu/menu_mem_fj.orig  menu/menu_mem_fj.dat
+    mv menu/menu_mem_us.orig  menu/menu_mem_us.dat
+
+    rm "${USB_ROOT}/revert_translation"
+
+    echo "Translation files reverted OK."
+
     poweroff
     exit
 fi
 
-cd "${USB_ROOT}"
-
-# Copy over the chimes
-# But first, let's make sure we actually have files to copy over
-if ! ls Chime/*.vag > /dev/null; then
-    echo 'Please copy the chimes from your Densha de Go! Final installation before using on your Densha de Go! Plug & Play.'
-    error_exit
-fi
-
-# Also, make sure (at least one) chime is correct
-if ! sha1sum -c chimes.sha1; then
-    echo 'Chimes hash mismatch. Please make sure to copy the correct chimes as per the README.'
-    error_exit
-fi
-
-# Make chimes directory
-mkdir -p /root/Data/cddata/dengo/Chime
-
-# Copy the files
-if ! cp Chime/*.vag /root/Data/cddata/dengo/Chime/; then
-    error_exit
-fi
-
-# Do patching
-# Copy bspatch to /tmp and apply execute permission
-cp bin/bspatch /tmp/bspatch
-chmod +x /tmp/bspatch
-
-cd /root
-
-# Check that the game executable is the expected version
-if ! sha1sum -c "${USB_ROOT}/dgf.sha1"; then
-    echo "Game executable SHA1 hash mismatch, this version may not be supported or the game has already been patched."
-    error_exit
-fi
-
-# Patch the game executable
-if ! LD_LIBRARY_PATH="${USB_ROOT}/bin" /tmp/bspatch dgf dgf_patched "${USB_ROOT}/dgf.patch"; then
-    echo "Error patching"
-    error_exit
-fi
-
-# Check that the patched executable is valid
-if ! sha1sum -c "${USB_ROOT}/dgf_patched.sha1"; then
-    echo "Patching appears to have produced incorrect file."
-    rm dgf_patched
-    rm -rf Data/cddata/dengo
-    error_exit
-fi
 
 # Move files into place
-mv dgf dgf.orig
-mv dgf_patched dgf
-chmod 555 dgf
-chmod 444 /root/Data/cddata/dengo/Chime/*
-chown 1000:1000 dgf
-chown -R 1000:1000 /root/Data/cddata/dengo
+cd /root/Data/cddata
+
+
+df -h
+
+echo "Log original files permissions..."
+ls -al 2d/*.orig
+ls -al common/*.orig
+ls -al menu/*.orig
+
+
+echo "Copying files..."
+cp "${USB_ROOT}/translation/cddata/2d/end_mem.dat"        2d/end_mem.dat
+cp "${USB_ROOT}/translation/cddata/2d/game2d_mem.dat"     2d/game2d_mem.dat
+cp "${USB_ROOT}/translation/cddata/2d/game2d_vram.dat"    2d/game2d_vram.dat
+cp "${USB_ROOT}/translation/cddata/common/com_mem.dat"    common/com_mem.dat
+cp "${USB_ROOT}/translation/cddata/menu/menu_mem_fj.dat"  menu/menu_mem_fj.dat
+cp "${USB_ROOT}/translation/cddata/menu/menu_mem_us.dat"  menu/menu_mem_us.dat
+
+
+echo "Changing files permissions..."
+chmod 664 2d/end_mem.dat
+chmod 664 2d/game2d_mem.dat
+chmod 664 2d/game2d_vram.dat
+chmod 664 common/com_mem.dat
+chmod 664 menu/menu_mem_fj.dat
+chmod 664 menu/menu_mem_us.dat
+
+chown 1000:1000 2d/end_mem.dat
+chown 1000:1000 2d/game2d_mem.dat
+chown 1000:1000 2d/game2d_vram.dat
+chown 1000:1000 common/com_mem.dat
+chown 1000:1000 menu/menu_mem_fj.dat
+chown 1000:1000 menu/menu_mem_us.dat
+
+
+
+echo "New files permissions..."
+ls -al 2d/*.dat
+ls -al common/*.dat
+ls -al menu/*.dat
 
 # We're done, shutdown
+echo "We're done, shutdown"
 poweroff
 
 } > "${USB_ROOT}/log.txt" 2>&1
